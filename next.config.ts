182e0f40f3600c136527ee484c0af6802daa4b10
jainamshah711 <jainam@beehively.com>
2025-09-25 21:10:05 +0530

    updated next config.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: false, // set true if you want a 308 permanent redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
